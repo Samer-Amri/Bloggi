147caa97706483c4547eed8695d08ffda8f00a10
Noxanoxa <samer_amri@univ-biskra.dz>
2025-03-05 09:26:33 +0100

    fix type in migration announcements

--- database/migrations/2024_09_26_172411_create_announcements_table.php
+++ database/migrations/2024_09_26_172411_create_announcements_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateAnnouncementsTable extends Migration
 {
@@ -19,11 +20,15 @@ class CreateAnnouncementsTable extends Migration
             $table->string('title_en')->index();
             $table->string('slug')->unique();
             $table->string('slug_en')->unique();
-            $table->longText('description')->index();
-            $table->longText('description_en')->index();
+            $table->text('description');
+            $table->text('description_en');
             $table->unsignedTinyInteger('status')->default(0);
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            // Specify key length for the description and description_en indexes
+            $table->index([DB::raw('description(255)')], 'announcements_description_index');
+            $table->index([DB::raw('description_en(255)')], 'announcements_description_en_index');
         });
     }
 
